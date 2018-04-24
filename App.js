0b038f3f8a4f179ca7da76cd99b54af6b94d8231
Hana Lee <i@leehana.co.kr>
2018-04-24 22:21:47 +0900

    위치 정보 가지고 오는 코드 추가. getCurrentPosition 은 안드로이드 에뮬에서 오류. 임시 회피 코드 추가

--- App.js
+++ App.js
@@ -1,45 +1,104 @@
-import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
-import Weather from "./Weather";
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Weather from './Weather';
 
 export default class App extends Component {
-  state = {
-    isLoaded: true
-  };
-  render() {
-    const { isLoaded } = this.state;
-    return (
-      <View style={styles.container}>
-        {isLoaded ? (
-          <Weather />
-        ) : (
-          <View style={styles.loading}>
-            <Text style={styles.loadingText}>
-              Getting the weather informations...
-            </Text>
-          </View>
-        )}
-      </View>
-    );
-  }
+	state = {
+		isLoaded: false
+	};
+
+	positionHandler(position) {
+		//state.isLoaded = true;
+		this.setState({
+			isLoaded: true
+		});
+	}
+	getPosition() {
+		this.useGetCurrentPositionFn(this.positionHandler);
+	}
+	useGetCurrentPositionFn(successHandler) {
+		navigator.geolocation.getCurrentPosition(
+			position => {
+				console.log('get current position', position);
+				// successHandler(position).bind(this);
+				this.setState({
+					isLoaded: true
+				});
+				// this.useWatchPositionFn(successHandler);
+			},
+			error => {
+				console.error('get current position error', JSON.stringify(error));
+				this.useWatchPositionFn(successHandler);
+			},
+			{
+				enableHighAccuracy: true,
+				timeout: 20000,
+				maximumAge: 1000,
+				distanceFilter: 10
+			}
+		);
+	}
+	useWatchPositionFn(successHandler) {
+		var watchId = navigator.geolocation.watchPosition(
+			position => {
+				console.log('watch position', position);
+				this.setState({
+					isLoaded: true
+				});
+				navigator.geolocation.clearWatch(watchId);
+			},
+			error => {
+				console.error('watch position error', JSON.stringify(error));
+				navigator.geolocation.clearWatch(watchId);
+			},
+			{
+				enableHighAccuracy: true,
+				timeout: 20000,
+				maximumAge: 1000,
+				distanceFilter: 10
+			}
+		);
+	}
+	componentDidMount() {
+		console.log('componentDidMount');
+		this.getPosition();
+	}
+
+	render() {
+		const { isLoaded } = this.state;
+		return (
+			<View style={styles.container}>
+				{isLoaded ? (
+					<Weather />
+				) : (
+					<View style={styles.loading}>
+						<Text style={styles.loadingText}>
+							Getting the weather informations...
+						</Text>
+					</View>
+				)}
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    justifyContent: "flex-start",
-    alignItems: "stretch",
-    flexWrap: "wrap"
-  },
-  loading: {
-    flex: 1,
-    backgroundColor: "#FDF6AA",
-    justifyContent: "flex-end",
-    paddingLeft: 25
-  },
-  loadingText: {
-    fontSize: 38,
-    marginBottom: 100
-  }
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		justifyContent: 'flex-start',
+		alignItems: 'stretch',
+		flexWrap: 'wrap'
+	},
+	loading: {
+		flex: 1,
+		backgroundColor: '#FDF6AA',
+		justifyContent: 'flex-end',
+		paddingLeft: 25,
+		paddingRight: 25
+	},
+	loadingText: {
+		fontSize: 38,
+		marginBottom: 24
+	}
 });
