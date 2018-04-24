829aa8b6850f8b3d0b84c777e8119c56c5320f68
Hana Lee <i@leehana.co.kr>
2018-04-24 14:56:30 +0900

    Initialized weather app by XDE

---
+++ App.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default class App extends Component {
+  state = {
+    isLoaded: false
+  };
+  render() {
+    const { isLoaded } = this.state;
+    return (
+      <View style={styles.container}>
+        {isLoaded ? null : (
+          <View style={styles.loading}>
+            <Text style={styles.loadingText}>
+              Getting the weather informations...
+            </Text>
+          </View>
+        )}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "flex-start",
+    alignItems: "stretch",
+    flexWrap: "wrap"
+  },
+  loading: {
+    flex: 1,
+    backgroundColor: "#FDF6AA",
+    justifyContent: "flex-end",
+    paddingLeft: 25
+  },
+  loadingText: {
+    fontSize: 38,
+    marginBottom: 100
+  }
+});
